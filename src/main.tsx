1d1d1e00e334f9f1994769f7ec934c3f399f0d55
yashparab17 <yash.sp@somaiya.edu>
2026-07-25 18:35:04 +0530

    Rebuilt main page, dark mode (no switcher yet), fixed index.css.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+
+document.documentElement.classList.add("dark");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
