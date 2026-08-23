9632ebae371af1054a53718133e4e95b6fe27ed7
yashparab17 <yash.sp@somaiya.edu>
2026-08-23 19:43:54 +0530

    Minor optimization (removed migrate.rs lol)

--- src-tauri/src/database/migrate.rs
+++
@@ -1,4 +0,0 @@
-//! Workspace migration placeholder module.
-//!
-//! Workspaces are currently constructed on the SQLite schema.
-//! This module is reserved for backward-compatible workspace import migrations.
