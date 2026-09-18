5eadb54b7b84de00838ce6277776e8e2b0e5cc0f
yashparab17 <yash.sp@somaiya.edu>
2026-09-18 10:13:17 +0530

    Enabled devtools.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,5 +3,13 @@
 
 /// Application entry point
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        // Fix WebKitGTK hardware acceleration / GBM buffer bug on Linux
+        if std::env::var("WEBKIT_DISABLE_DMABUF_RENDERER").is_err() {
+            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        }
+    }
+
     nexsync_lib::run()
 }
